ccdd997cf5217eaeed6463f2cf65d75a708deb58
Gideon Bar <gideon.bar@pepticom.com>
2022-10-12 23:48:10 +0300

    Created basic functional Python pyenv build support Added build automation script updated iml suggestions necessary gitignore regexes Renamed notebooks using Standard Camel Case Added Intellij support

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='pep_data',
+      version='0.1',
+      description='A repo for data exploration python sdk and notebooks',
+      url='git@github.com:Pepticom/pep-data.git',
+      author='gideonbar',
+      author_email='gideon.bar@pepticom.com',
+      license='Private',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['pyspark==3.0.1', 'matplotlib-inline==0.1.6', 'pandas==1.5.0']
+      )
