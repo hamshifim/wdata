1ec9960340f8451979ffa93184ef64972202c235
Gideon Bar <gideon.bar@pepticom.com>
2022-11-25 14:21:07 +0200

    complyence with python 3.8

--- src/pep_data/spark/util.py
+++ src/pep_data/spark/util.py
@@ -5,14 +5,24 @@ def field_to_struct(header, floats=[], doubles=[], integers=[], booleans=[]):
 
     field_type = StringType()
 
-    match header:
-        case a if a in floats:
-            field_type = FloatType()
-        case a if a in integers:
-            field_type = IntegerType()
-        case a if a in doubles:
-            field_type = DoubleType()
-        case a if a in booleans:
-            field_type = BooleanType()
+    if header in floats:
+        field_type = FloatType()
+    elif header in integers:
+        field_type = IntegerType()
+    elif header in doubles:
+        field_type = DoubleType()
+    elif header in booleans:
+        field_type = BooleanType()
+
+# TODO replace the above with this when Wielder supports the new python 3.10
+    # match header:
+    #     case a if a in floats:
+    #         field_type = FloatType()
+    #     case a if a in integers:
+    #         field_type = IntegerType()
+    #     case a if a in doubles:
+    #         field_type = DoubleType()
+    #     case a if a in booleans:
+    #         field_type = BooleanType()
 
     return StructField(header, field_type)
