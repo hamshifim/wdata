cd009181af4a8088f8abc68339829446cae83ec3
pep-dima <dmitriy.chikalovets@pepticom.com>
2022-12-12 17:31:14 +0200

    Added PyTorch examples for jupyter. Updated zeppelin script. Updated zeppelin notebooks

--- src/zep/zeppelin.sh
+++ src/zep/zeppelin.sh
@@ -1,5 +1,2 @@
 
-docker run -u $(id -u) -p 8080:8080 -p 4040:4040 --rm -v $HOME/docker-spark/spark-3.1.3-bin-hadoop2.7:/opt/spark -v $HOME/dev/peps:/dev/peps -v $HOME/dev/buckets:/dev/buckets -e SPARK_HOME=/opt/spark  --name zeppelin apache/zeppelin:0.10.0
-
-
-
+docker run -u $(id -u) -p 8080:8080 -p 4040:4040 --rm -v $HOME/docker-spark/spark-3.1.3-bin-hadoop2.7:/opt/spark -v $HOME/dev/duds/:/dev/duds -v $HOME/dev/buckets:/dev/buckets -e SPARK_HOME=/opt/spark --network host --name zeppelin apache/zeppelin:0.10.0
