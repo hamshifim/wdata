65b70b2a111905d1f487aa96c23320aa61c41e3b
PeninaSchuss <pninast@edu.hac.ac.il>
2023-05-07 11:20:37 +0300

    covering some functions so it will work in Windows

---
+++ src/pep_data/test/t_conf_windows.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from pep_data.project import data_project_conf
+from wielder.wield.remote import configure_remote_unique_context
+
+
+if __name__ == "__main__":
+
+    conf = data_project_conf()
+
+    configure_remote_unique_context(conf)
